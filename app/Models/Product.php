3bc5a04e8fcfcaeda5ad7d2eab6d6cd3c4c3c96d
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-22 16:29:13 +0700

    feat(md): add `Product` and `Supplier`

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable([
+    'name', 
+    'product_type_id', 
+    'base_unit', 
+    'category_id', 
+    'description', 
+    'selling_price', 
+    'stock_quantity', 
+    'expiration_date', 
+    'image', 
+    'is_active'
+])]
 class Product extends Model
 {
-    //
+    public function productType()
+    {
+        return $this->belongsTo(ProductType::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function suppliers()
+    {
+        return $this->belongsToMany(Supplier::class);
+    }
 }
--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable(['name', 'phone', 'address', 'is_active'])]
 class Supplier extends Model
 {
-    //
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }

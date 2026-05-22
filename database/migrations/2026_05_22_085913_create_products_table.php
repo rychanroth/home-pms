e3333b2cc8cc88cb27cadc7138576c6df0d207ff
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-22 16:03:52 +0700

    feat(mgr): for table `products` and `suppliers` and junction table

---
+++ database/migrations/2026_05_22_085913_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('product_type_id')->nullable()->constrained()->nullOnDelete();  
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();  
+            $table->string('base_unit')->default('tablet'); // tablet, capsule, bottle, etc.  
+            $table->text('description')->nullable();  
+            $table->decimal('selling_price', 10, 2);
+            $table->integer('stock_quantity')->default(0);  
+            $table->date('expiration_date')->nullable();  
+            $table->string('image')->nullable();  
+            $table->boolean('is_active')->default(true);  
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
